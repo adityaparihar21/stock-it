4a589ce369057d0c3541edc84f5da822756c8e31
Aditya Parihar <parihar21aditya@gmail.com>
2026-07-24 13:15:40 +0530

    Update DB variable name to MONGODB_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/product_inventory";
+    const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/product_inventory";
     const conn = await mongoose.connect(mongoURI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
